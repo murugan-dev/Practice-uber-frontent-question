d09f2b5fc0586eb6391aeb7568c0be72634d1390
murugan-dev <murugankumarasamy897@gmail.com>
2024-07-05 14:31:31 -0400

    BarChart problem Completed

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,34 +1,51 @@
 import React, { useMemo } from "react";
+import {motion} from "framer-motion"
 
-function BarData({ data, len }) {
+function BarData({ data, highestCount }) {
   return (
-    <div
+    <motion.div
       className="bar"
+      initial = {{ height: 0}}
+      animate = {{ height: `${highestCount}%` }}
+      exit = {{ height:  0}}
       style={{
-        height: `${data.ticketCount}%`,
+        
         background: data.colour,
-        width: `${100 / len}%`,
+
       }}
       key={data.name}
     >
-      {data.name}
-    </div>
+      <pre className="category">
+      
+
+      {data.name} :  {data.ticketCount}
+      </pre>
+    </motion.div>
   );
 }
 
 function BarChart({ data }) {
   const lengthList = useMemo(() => data.length, [data]);
+  const maxTicketCount = useMemo(()=>{
+    return Math.max(...data.map((item)=>item.ticketCount))
+  },[data])
+  console.log(maxTicketCount)
   return (
-    <div className="barChart-container">
+    <motion.div className="barChart-container"
+      initial = {{opacity : 0}}
+      animate = {{ opacity: 1}}
+      exit={{opacity:0}}
+      transition={{duration: 0.5}}
+    >
       <div className="axis"></div>
       <h3 className="tickets">Numbers of tickets</h3>
       <h3 className="department">Departments</h3>
       <div className="chart">
         {data.map((item, idx) => (
-          <BarData key={idx} data={item} len={lengthList} />
+          <BarData key={idx} data={item} len={lengthList} highestCount={(item.ticketCount / maxTicketCount)*100} />
         ))}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
