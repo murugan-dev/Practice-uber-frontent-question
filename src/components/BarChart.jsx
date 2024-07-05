fe07e31c0208ed06b78dba4ad8a47bfaa7a61358
murugan-dev <murugankumarasamy897@gmail.com>
2024-07-05 11:06:20 -0400

    show bar on the graph

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, { useMemo } from "react";
 
-function BarData({ datas }) {
+function BarData({ data, len }) {
   return (
-    <div 
-    className='bar'
+    <div
+      className="bar"
       style={{
-        height: `${datas.ticketCount}%`,
-        background: datas.colour,
-        width: '50px'
-      }} 
-      key={datas.name}
+        height: `${data.ticketCount}%`,
+        background: data.colour,
+        width: `${100 / len}%`,
+      }}
+      key={data.name}
     >
-      {datas.name}
+      {data.name}
     </div>
   );
 }
 
 function BarChart({ data }) {
+  const lengthList = useMemo(() => data.length, [data]);
   return (
-    <div className='barChart-container'>
-      <div className='axis'></div>
-      <h3 className='tickets'>Numbers of tickets</h3>
-      <h3 className='department'>Departments</h3>
-      <div className='chart'>
-      {
-        data.map((item, idx) => (
-          <BarData key={idx} datas={item}/>
-        ))
-      }
+    <div className="barChart-container">
+      <div className="axis"></div>
+      <h3 className="tickets">Numbers of tickets</h3>
+      <h3 className="department">Departments</h3>
+      <div className="chart">
+        {data.map((item, idx) => (
+          <BarData key={idx} data={item} len={lengthList} />
+        ))}
       </div>
     </div>
   );
